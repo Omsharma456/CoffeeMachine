e27041b7fcc35beab1df7c7a4016be0cbc2bb29b
Prashant18290 <Prashant.dubey_cs18@gla.ac.in>
2020-03-12 03:44:08 +0530

    Scanner keyword to take input

--- src/Main/Action.java
+++ src/Main/Action.java
@@ -2,10 +2,12 @@ package Main;
 
 import DefinitionMachine.CoffeeMachine;
 
+import java.util.Scanner;
+
 public class Action extends CoffeeMachine {
     public static void main(String[] args) {
         while (true) {
-
+            Scanner sc = new Scanner(System.in);
         }
     }
 }
