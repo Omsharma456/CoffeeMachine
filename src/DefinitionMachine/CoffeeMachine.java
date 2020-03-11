38e50bd4f43cd6233a92b83156cd5fd700aefcd7
Prashant18290 <Prashant.dubey_cs18@gla.ac.in>
2020-03-12 03:32:07 +0530

    This method shows remaining Quantity ml: Quantity available of water Quantity available of Milk Quantity available of Beans Quantity available of disposable cups
    total money credited.

--- src/DefinitionMachine/CoffeeMachine.java
+++ src/DefinitionMachine/CoffeeMachine.java
@@ -9,4 +9,13 @@ public class CoffeeMachine {
     public static int InitialBeans = 120;
     public static int disposable_cups = 9;
     public static int totalMoney = 550;
+
+    public static void Remaining() {
+        System.out.println("\nThe coffee machine has:");
+        System.out.println(InitialWater + " of water");
+        System.out.println(InitialMilk + " of milk");
+        System.out.println(InitialBeans + " of coffee beans");
+        System.out.println(disposable_cups + " of disposable cups");
+        System.out.println("$" + totalMoney + " of money");
+    }
 }
