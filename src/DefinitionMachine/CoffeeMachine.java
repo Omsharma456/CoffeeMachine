2c5990e7de57d6cc6579ca0a917c329b09ca9247
Prashant18290 <Prashant.dubey_cs18@gla.ac.in>
2020-03-12 03:35:07 +0530

    This Buy method is mainly used for buying three type of Coffee available in CoffeeMachine. 1. espresso     -$4 2. latte        -$7 3. cappuccino   -$6

--- src/DefinitionMachine/CoffeeMachine.java
+++ src/DefinitionMachine/CoffeeMachine.java
@@ -18,4 +18,48 @@ public class CoffeeMachine {
         System.out.println(disposable_cups + " of disposable cups");
         System.out.println("$" + totalMoney + " of money");
     }
+
+    public static void Buy() {
+        System.out.println("\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
+        String need = sc.next();
+        switch (need) {
+            case "1":
+                if (InitialWater >= 250 && InitialBeans >= 16) {
+                    System.out.println("I have enough resources, making you a coffee!");
+                    InitialWater -= 250;
+                    InitialBeans -= 16;
+                    disposable_cups -= 1;
+                    totalMoney += 4;
+                } else {
+                    System.out.println("Sorry, not enough water!");
+                }
+                break;
+
+            case "2":
+                if (InitialWater >= 350 && InitialMilk >= 75 && InitialBeans >= 20) {
+                    System.out.println("I have enough resources, making you a coffee!");
+                    InitialWater -= 350;
+                    InitialMilk -= 75;
+                    InitialBeans -= 20;
+                    disposable_cups -= 1;
+                    totalMoney += 7;
+                } else {
+                    System.out.println("Sorry, not enough water!");
+                }
+                break;
+
+            case "3":
+                if (InitialWater >= 200 && InitialMilk >= 100 && InitialBeans >= 12) {
+                    System.out.println("I have enough resources, making you a coffee!");
+                    InitialWater -= 200;
+                    InitialMilk -= 100;
+                    InitialBeans -= 12;
+                    disposable_cups -= 1;
+                    totalMoney += 6;
+                } else {
+                    System.out.println("Sorry, not enough water!");
+                }
+                break;
+        }
+    }
 }
