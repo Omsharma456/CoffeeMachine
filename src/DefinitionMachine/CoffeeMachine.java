9360fc012cdc15a0f32cc57454c086b40b25e8bd
Prashant18290 <Prashant.dubey_cs18@gla.ac.in>
2020-03-12 03:28:18 +0530

    InitialMilk available in CoffeeMachine

--- src/DefinitionMachine/CoffeeMachine.java
+++ src/DefinitionMachine/CoffeeMachine.java
@@ -5,4 +5,5 @@ import java.util.Scanner;
 public class CoffeeMachine {
     static Scanner sc = new Scanner(System.in);
     public static int InitialWater = 400;
+    public static int InitialMilk = 540;
 }
